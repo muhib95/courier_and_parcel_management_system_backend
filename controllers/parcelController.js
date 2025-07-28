808ba7166797279ff6b5260d7e9145a3a81b8a6f
Muhibbul Hasan <34686084+muhib95@users.noreply.github.com>
2025-07-28 23:00:47 +0600

    view single booking parcel

--- controllers/parcelController.js
+++ controllers/parcelController.js
@@ -66,5 +66,31 @@ const getMyParcels = async (req, res) => {
     res.status(500).json({ success: false, message: 'Failed to fetch bookings' });
   }
 };
+const getParcelById = async (req, res) => {
+  try {
+    const parcelId = req.params.id;
+    const userId = req.user.id;
+    const userRole = req.user.role;
 
-module.exports = { bookParcel, getMyParcels };
+    // Find parcel by ID
+    const parcel = await Parcel.findById(parcelId);
+
+    if (!parcel) {
+      return res.status(404).json({ success: false, message: 'Parcel not found' });
+    }
+
+    // Authorization: 
+    // Customer can only view their own parcel
+    // Agent/Admin can view all parcels
+    if (userRole === 'customer' && parcel.customer.toString() !== userId) {
+      return res.status(403).json({ success: false, message: 'Access denied' });
+    }
+
+    res.status(200).json({ success: true, parcel });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Failed to get parcel' });
+  }
+};
+
+module.exports = { bookParcel, getMyParcels, getParcelById};
--- routes/parcel.js
+++ routes/parcel.js
@@ -1,10 +1,11 @@
 // routes/parcels.js
 const express = require('express');
 const router = express.Router();
-const { bookParcel, getMyParcels } = require('../controllers/parcelController');
+const { bookParcel, getMyParcels, getParcelById } = require('../controllers/parcelController');
 const authMiddleware = require('../middleware/auth');
 
 router.get('/booking/history', authMiddleware, getMyParcels);
+router.get('/single/booking/history/:id', authMiddleware, getParcelById);
 router.post('/book', authMiddleware, bookParcel);
 
 module.exports = router;
